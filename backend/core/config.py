f69e0a6fc6781497e401e9d5e0e6f035ed10399a
unknown <kibogora.nsoro@gmai.com>
2024-07-21 14:48:36 +0200

    Add remaining files for db connection

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,22 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.1"
+    
+    MYSQL_USER: str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD")
+    MYSQL_SERVER: str = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_PORT: str = os.getenv("MYSQL_PORT", 3306)
+    MYSQL_DB: str = os.getenv("MYSQL_DB", "blogapplication")
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"mysql://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}@{self.MYSQL_SERVER}:{self.MYSQL_PORT}/{self.MYSQL_DB}"
     
 settings = Settings()
